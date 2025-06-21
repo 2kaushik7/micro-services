369fd6d00e34d5e4d637149c5b64a26677c30de1
kaushik.sst@gmail.com <kaushik.sst@gmail.com>
2025-06-21 15:33:47 +0100

    Build cards, loans, accounts services

---
+++ module-2/accounts/src/test/java/com/systemsai/finservices/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemsai.finservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
