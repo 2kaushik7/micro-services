f0b15c2d32d4ec9419f4fd2f2c7244c7f7d2239f
kaushik.sst@gmail.com <kaushik.sst@gmail.com>
2025-06-21 15:23:56 +0100

    Added cards, loans, accounts services

---
+++ module-1/accounts/src/main/java/com/systemsai/finservices/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.systemsai.finservices.mapper;
+
+import com.systemsai.finservices.accounts.entity.Customer;
+import com.systemsai.finservices.dto.CustomerDto;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
